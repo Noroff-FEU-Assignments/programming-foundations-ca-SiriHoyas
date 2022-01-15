ef1780d6916d713717e36653088f2cde7bd87e74
Siri Høyås <siri@skjervold.me>
2022-01-15 11:34:22 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,14 +44,53 @@ let games = [
    ];
 
    for (i = 0; i < games.length; i++) {
-       if (games.rating <= 3.5) {
-        gamesList.innerHTML + "<li>" + games.title + "</li>";
+        const gameRating = games[i].rating
+        const gameTitle = games[i].title
+       if (gameRating <= 3.5) {
+        gamesList.innerHTML + "<li>" + gameTitle + "</li>";
        }
    }
 
+   //Denne må jeg se mer på!!!!
+
 // Question 4
 
+function whatIDontLike (wakeUpCall) {
+    if (typeof wakeUpCall === "string") {
+        console.log("I dont like " + wakeUpCall);
+    } else {
+        console.log("Please send in a string");
+    }
+}
+
+whatIDontLike("Getting up early");
+whatIDontLike(true);
+whatIDontLike(3); 
+whatIDontLike();
+
+//.toLowerCase
+
+
 // Question 5
+function myFunction(argument1, argument2) {
+let convertedNumber1 = parseInt(argument1);
+let convertedNumber2 = parseInt(argument2);
+
+if (isNaN(convertedNumber1) || isNaN(convertedNumber2)) {
+    return "Invalid Arguments";
+} else {
+    return convertedNumber1 - convertedNumber2;
+}
+}
+
+document.getElementById("subtraction").innerHTML = myFunction(1, 42);
+document.getElementById("subtraction").innerHTML = myFunction(200, 150);
+document.getElementById("subtraction").innerHTML = myFunction(10, "50");
+document.getElementById("subtraction").innerHTML = myFunction("100", "400");
+document.getElementById("subtraction").innerHTML = myFunction("Ten", "One Hundred");
+document.getElementById("subtraction").innerHTML = myFunction(null, 123);
+
+
 
 // Question 6
 
